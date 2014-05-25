53ad4bc6ad843a66f576052bfaaa01f506c1228f
qingfeng <jingxing05@163.com>
2014-05-25 21:41:39 +0800

    search list  , view movie , view celebrity ok

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('deviceready', onDeviceReady, false);
-var douban_api_url = "https://api.douban.com/v2/movie/";
+var douban_api_url = 'https://api.douban.com/v2/movie/';
+douban_api_key = '0cfdeb465927f92f26f9c1d30b77eb8d';
 function onDeviceReady() {
   navigator.splashscreen.hide();
   //注册后退按钮
@@ -19,7 +20,32 @@ function onDeviceReady() {
       }
     }
   }, false);
-}
+};
+var view_movieid = null;
+var view_celebrityid = null;
+//处理电影搜索列表
+function parseMovielist(json) {
+  var html = '';
+  if (json.total == 0) {
+    J.hideMask();
+    J.showToast('没搜到相关电影:(', 'error');
+  } else {
+    $.each(json.subjects, function (i, item) {
+      html += '<li id="' + item.id + '" data-selected="selected">'
+        + '<a href="#movie_section" data-target="section">'
+        + '<strong>' + item.title + '</strong>'
+        + '<p>评分:<span class="label">' + item.rating.average + '/' + item.rating.max + '</span>' + parseInt(item.rating.stars) + '&hearts;</p>'
+        + '<p>年份:' + item.year + '</p></a>'
+        + '<span><img src="' + item.images.small + '?apikey=' + douban_api_key + '" alt="' + item.title + '" title="' + item.title + '"/></span>'
+        + '</li>';
+    });
+    $('#movie_search_list').html(html);
+    J.Scroll('#index_section article');//刷新滚动条
+    J.hideMask();
+  }
+  return false;
+};
+
 var App = (function () {
   var pages = {};
   var run = function () {
@@ -106,8 +132,10 @@ var App = (function () {
     calcChartOffset: calcChartOffset
   }
 }());
+
 App.page('index', function () {
   this.init = function () {
+    //搜索关键字
     $('#search_btn').on('tap', function () {
       var search_key = $('#input_search_key').val().trim();
       if (search_key.length == 0) {
@@ -122,39 +150,94 @@ App.page('index', function () {
       } else {
         J.showMask();
         //这里可以去抓取数据了 url, data, success, dataType
-        $.getJSON(douban_api_url + 'search?q=' + search_key + '&apikey=0cfdeb465927f92f26f9c1d30b77eb8d&count=10&callback=?', function(remoteData){
+        $.getJSON(douban_api_url + 'search?q=' + search_key + '&apikey=' + douban_api_key + '&count=10&callback=?', function (remoteData) {
           parseMovielist(remoteData)
         });
       }
     });
+
+
+    //点击电影条目
+    $('#movie_search_list').on('tap', function (e) {
+      var obj = e.target || e.srcElement;
+      view_movieid = $(obj).parents('li').attr('id');
+    });
+
+    $('#btn_show_welcome').on('tap', function () {
+      console.log('shioiiiit ititiiti ');
+
+      //$('#btn_show_welcome').attr('href', '#movie_seciton');
+    });
+
+
   }
 });
-App.page('form', function () {
-  this.init = function () {
-    $('#checkbox_1').on('change', function () {
-      alert($(this).data('checkbox'));
-    })
+
+App.page('movie', function () {
+  //每次show的时候，加载数据更新模板数据
+  this.show = function () {
+    if (view_movieid && parseInt(view_movieid) > 0) {
+      var viewedid = $('#moviedetail').attr('viewed');
+      if (viewedid != view_movieid) {
+        J.showMask();
+        //获取是哪部电影哦
+        $.getJSON(douban_api_url + 'subject/' + view_movieid + '?apikey=' + douban_api_key + '&callback=?', function (remoteData) {
+          J.tmpl('#movie_section', 'movie_tpl', remoteData);
+          //点击影人条目
+          $('.celebrity').on('tap', function (e) {
+            var obj = e.target || e.srcElement;
+            var celebrityid = $(obj).parents('.celebrity').attr('id');
+            view_celebrityid = celebrityid;
+            if (parseInt(celebrityid) == 0) {
+              return false;
+            }
+            J.showMask();
+            $.getJSON(douban_api_url + 'celebrity/' + celebrityid + '?apikey=' + douban_api_key + '&callback=?', function (remoteData) {
+              J.tmpl('#celebrity_section', 'celebrity_tpl', remoteData);
+              $('.celebritywork').on('tap', function (e) {
+                var obj = e.target || e.srcElement;
+                view_movieid = $(obj).parents('.celebritywork').attr('id');
+                console.log(view_movieid);
+              });
+              J.Scroll('#celebritydetail');//刷新滚动条
+              J.hideMask();
+            });
+          });
+
+          J.Scroll('#moviedetail');//刷新滚动条
+          J.hideMask();
+        });
+      }
+
+    } else {
+      window.history.go(-1);
+    }
   }
 });
+
+App.page('celebrity', function () {
+  //每次show的时候，加载数据更新模板数据
+  this.show = function () {
+    if (view_celebrityid == null || parseInt(view_celebrityid) == 0) {
+      window.history.go(-1);
+    }
+  }
+});
+
+
+App.page('viewhistory', function () {
+  //每次show的时候，加载数据更新模板数据
+  this.show = function () {
+    //cong localstorage里面 查询数据
+
+
+    if (view_celebrityid == null || parseInt(view_celebrityid) == 0) {
+      window.history.go(-1);
+    }
+  }
+});
+
+
 $(function () {
   App.run();
 });
-
-function parseMovielist(json) {
-  var html = '';
-  if(json.count>0){
-    $('#no_movies_tip').hide();
-  }
-  $.each(json.subjects, function (i, item) {
-    html += '<li data-icon="next" data-selected="selected"><i class="icon next"></i>'
-      + '<a href="" data-target="section">'
-      + '<strong>' + item.title + '</strong>'
-      + '<p>评分:<span class="label">' + item.rating.average +'</span>最高:' + item.rating.max + '分，共'+item.rating.stars + '人评分</p>'
-      + '<p>年份:' + item.year + '</p></a>'
-      + '<span><img src="' + item.images.small + ' alt="' + item.title + '" title="' + item.title + '"/></span></li>';
-  });
-
-  $('#movie_search_list').html(html);
-  J.hideMask();
-  return false;
-};
